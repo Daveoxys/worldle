683a8fb7fa0d718e2de4d9b05b0539cf7f1f2dae
Dave Taylor <davidtaylor970@googlemail.com>
2022-03-03 16:05:26 +0000

    Keyboard/Game testing

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import "../styles/App.css";
 import NavBar from "./NavBar";
 import Map from "./Map";
 import Keyboard from "./Keyboard";
+import Game from "./Game";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Map />
+      <Game />
       <Keyboard />
     </div>
   );
