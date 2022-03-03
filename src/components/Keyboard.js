dda550287da30c344d50613600c70279532a153d
Dave Taylor <davidtaylor970@googlemail.com>
2022-03-03 15:24:18 +0000

    Keyboard testing

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -5,38 +5,42 @@ const Keyboard = () => {
   return (
     <div className="keyboard">
       <div class="keyboard-top">
-        <button data-key="q">Q</button>
-        <button data-key="w">W</button>
-        <button data-key="e">E</button>
-        <button data-key="r">R</button>
-        <button data-key="t">T</button>
-        <button data-key="y">Y</button>
-        <button data-key="u">U</button>
-        <button data-key="i">I</button>
-        <button data-key="o">O</button>
-        <button data-key="p">P</button>
+        <button data-key="q">q</button>
+        <button data-key="w">w</button>
+        <button data-key="e">e</button>
+        <button data-key="r">r</button>
+        <button data-key="t">t</button>
+        <button data-key="y">y</button>
+        <button data-key="u">u</button>
+        <button data-key="i">i</button>
+        <button data-key="o">o</button>
+        <button data-key="p">p</button>
       </div>
       <div class="keyboard-middle">
-        <button data-key="a">A</button>
-        <button data-key="s">S</button>
-        <button data-key="d">D</button>
-        <button data-key="f">F</button>
-        <button data-key="g">G</button>
-        <button data-key="h">H</button>
-        <button data-key="j">J</button>
-        <button data-key="k">K</button>
-        <button data-key="l">L</button>
+        <button data-key="a">a</button>
+        <button data-key="s">s</button>
+        <button data-key="d">d</button>
+        <button data-key="f">f</button>
+        <button data-key="g">g</button>
+        <button data-key="h">h</button>
+        <button data-key="j">j</button>
+        <button data-key="k">k</button>
+        <button data-key="l">l</button>
       </div>
       <div class="keyboard-bottom">
-        <button data-key="enter">ENTER</button>
-        <button data-key="a">Z</button>
-        <button data-key="s">X</button>
+        <button data-key="enter" class="big-button">
+          ent
+        </button>
+        <button data-key="a">z</button>
+        <button data-key="s">x</button>
         <button data-key="d">C</button>
         <button data-key="f">V</button>
-        <button data-key="g">B</button>
-        <button data-key="h">N</button>
-        <button data-key="j">M</button>
-        <button data-key="backspace">DELETE</button>
+        <button data-key="g">b</button>
+        <button data-key="h">n</button>
+        <button data-key="j">m</button>
+        <button data-key="backspace" class="big-button">
+          del
+        </button>
       </div>
     </div>
   );
