0d20fb303543eb31f7914c5cf1d9b31d790aa71b
Dave Taylor <davidtaylor970@googlemail.com>
2022-03-06 18:08:21 +0000

    created game, constants, keyboard and css

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,87 +1,87 @@
-import React, { useState } from "react";
-import Alert from "./Alert";
-import continents from "../data/continents.json";
-import "../styles/Game.css";
+// import React, { useState } from "react";
+// import Alert from "./Alert";
+// import continents from "../data/continents.json";
+// import "../styles/Game.css";
 
-const Game = () => {
-  const getRandomCountry = () => {
-    const randomIndex = Math.floor(Math.random() * continents.length);
-    return continents[randomIndex];
-  };
+// const Game = () => {
+//   const getRandomCountry = () => {
+//     const randomIndex = Math.floor(Math.random() * continents.length);
+//     return continents[randomIndex];
+//   };
 
-  // const getCountries = (continent) =>
-  //   Object.values(json).filter(
-  //     (country) => country.continent === continent
-  //   );
+//   // const getCountries = (continent) =>
+//   //   Object.values(json).filter(
+//   //     (country) => country.continent === continent
+//   //   );
 
-  // const getRandomCountry = (continent) => {
-  //   const countries = getCountries(continent);
-  //   const randomIndex = Math.ceil(Math.random() * countries.length);
-  //   return countries[randomIndex];
-  // };
+//   // const getRandomCountry = (continent) => {
+//   //   const countries = getCountries(continent);
+//   //   const randomIndex = Math.ceil(Math.random() * countries.length);
+//   //   return countries[randomIndex];
+//   // };
 
-  const initialState = {
-    alert: {
-      message: "",
-      success: false,
-    },
-  };
+//   const initialState = {
+//     alert: {
+//       message: "",
+//       success: false,
+//     },
+//   };
 
-  const [alert, setAlert] = useState(initialState.alert);
-  const [guesses, setGuesses] = useState(5);
+//   const [alert, setAlert] = useState(initialState.alert);
+//   const [guesses, setGuesses] = useState(5);
 
-  const decrementGuesses = () => {
-    if (guesses === 0) {
-      return;
-    }
-    setGuesses((prevGuesses) => prevGuesses - 1);
-  };
+//   const decrementGuesses = () => {
+//     if (guesses === 0) {
+//       return;
+//     }
+//     setGuesses((prevGuesses) => prevGuesses - 1);
+//   };
 
-  const handleGuess = (event) => {
-    event.preventDefault();
-    //if usersGuess === JSON answer
-    //setAlert({ message: "Well done!", success: true });
-    //renderCity && reset initialState
+//   const handleGuess = (event) => {
+//     event.preventDefault();
+//     //if usersGuess === JSON answer
+//     //setAlert({ message: "Well done!", success: true });
+//     //renderCity && reset initialState
 
-    //if usersGuess !== JSON answer
-    //attempts remaining -1
+//     //if usersGuess !== JSON answer
+//     //attempts remaining -1
 
-    if (guesses === 1) {
-      setAlert({
-        message: `Incorrect - you have ${guesses} life remaining`,
-        success: false,
-      });
-    } else {
-      setAlert({
-        message: `Incorrect - you have ${guesses} lives remaining`,
-        success: false,
-      });
-    }
-  };
+//     if (guesses === 1) {
+//       setAlert({
+//         message: `Incorrect - you have ${guesses} life remaining`,
+//         success: false,
+//       });
+//     } else {
+//       setAlert({
+//         message: `Incorrect - you have ${guesses} lives remaining`,
+//         success: false,
+//       });
+//     }
+//   };
 
-  return (
-    <div className="Game">
-      <form onSubmit={handleGuess}>
-        <Alert message={alert.message} success={alert.success} />
-        <input placeholder="Country" />
-        <button className="submit-guess" onClick={decrementGuesses}>
-          Submit
-        </button>
-      </form>
+//   return (
+//     <div className="Game">
+//       <form onSubmit={handleGuess}>
+//         <Alert message={alert.message} success={alert.success} />
+//         <input placeholder="Country" />
+//         <button className="submit-guess" onClick={decrementGuesses}>
+//           Submit
+//         </button>
+//       </form>
 
-      {/* <div className="countries">
-        {Continents.map((country) => {
-          if (country.continent === "na") {
-            return (
-              <h4>
-                {country.country}, {country.city}
-              </h4>
-            );
-          }
-        })}
-      </div> */}
-    </div>
-  );
-};
+//       {/* <div className="countries">
+//         {Continents.map((country) => {
+//           if (country.continent === "na") {
+//             return (
+//               <h4>
+//                 {country.country}, {country.city}
+//               </h4>
+//             );
+//           }
+//         })}
+//       </div> */}
+//     </div>
+//   );
+// };
 
-export default Game;
+// export default Game;
