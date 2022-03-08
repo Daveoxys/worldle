eb73f4b02ada5441b79b374ff005f0fc9aeb3bc5
Nathan Horne <n.horne90@hotmail.co.uk>
2022-03-08 14:38:21 +0000

    updated game instructions on popup

--- src/components/App.js
+++ src/components/App.js
@@ -33,19 +33,10 @@ function App() {
         <h3>Welcome to Worldle!</h3>
         <p>Conquer the world by guessing the correct city in each continent!</p>
         <p>
-
           You will get five tries to correctly guess the city in each continent,
           after each try any correct letters in your guess will show how close
-          you were to the correct answer.
-
-          Conquer the world by guessing the correct country and capital city in
-          each continent!
-        </p>
-        <p>
-          You will get 5 tries to correctly guess the country and capital city
-          in each continent, after each try any correct letters in your guess
-          will show how close you were to the correct answer.
-
+          you were to the correct answer. Conquer the world by guessing the
+          correct country and capital city in each continent!
         </p>
         <hr />
         <p>Worldle will refresh new answers every day!</p>
