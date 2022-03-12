5f8c849294d8597a0f46cbaf1ae8313fc5943f57
Dave Taylor <davidtaylor970@googlemail.com>
2022-03-12 18:37:03 +0000

    popup improvement and words added

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,4 +1,7 @@
 import React from "react";
+import present from "../data/images/Present.png";
+import correct from "../data/images/Correct.png";
+import wrong from "../data/images/Wrong.png";
 import "../styles/Popup.css";
 
 function Popup(props) {
@@ -9,17 +12,24 @@ function Popup(props) {
           Close
         </button>
         {props.children}
-
         <h3>Welcome to Worldle!</h3>
-        <p>Conquer the world by guessing the correct city in each continent!</p>
+        <p>Guess a six-letter city from one of the world's continents.</p>
         <p>
-          You will get five tries to correctly guess the city in each continent,
-          after each try any correct letters in your guess will show how close
-          you were to the correct answer. Conquer the world by guessing the
-          correct country and capital city in each continent!
+          You will have five attempts to correctly guess the city in each
+          continent. After each try any correct letters in your guess will show
+          how close you were to the correct answer.
         </p>
         <hr />
-        <p>Worldle will refresh new answers every day!</p>
+        <h3>Examples</h3>
+        <img src={correct} alt="correct" />
+        <p>The letter S is in the city name and the correct spot.</p>
+        <img src={present} alt="present" />
+        <p>The letter S is in the city name but the wrong spot.</p>
+        <img src={wrong} alt="wrong" />
+        <p>None of the letters are in the city name.</p>
+        <br />
+        <hr />
+        <p>Conquer the world by guessing the correct city in each continent!</p>
       </div>
     </div>
   ) : (
